287cccf0f77a7bd7539df2dece89d0e1d63764a8
Laila Campbell <lailacampbell96@gmail.com>
2020-02-24 04:37:36 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,7 +10,7 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
-groceries.values.flatten
+groceries.values
  groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
   groceries.values
 end
